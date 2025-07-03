a29e3f47ce23d761d2c2618506b3aaf00017ea4c
dimple18 <135057748+dimp01@users.noreply.github.com>
2025-07-03 16:54:59 +0000

    Updated finance_data

--- project/project1/main.py
+++ project/project1/main.py
@@ -5,11 +5,34 @@ from pathlib import Path
 
 base = Path(__file__).resolve().parent
 
-with open(f'{base}/finance_data.xlsx', 'r', encoding='utf-8', errors='replace') as f:
-    lines = f.readlines()
-
-with open(f'{base}/new_fin_data.xlsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-
-df = pd.read_excel(f'{base}/new_fin_data.xlsx')
+df = pd.read_excel(f'{base}/finance_data.xlsx'); 
 print(df.head(5))
+
+print(df.columns)
+print(df.info())
+print(df.describe().T)
+print("Null per column:\n", df.isnull().sum())
+
+
+# tweaking data
+df["Burn_Rate"] = df["Marketing_Spend"] + df["Expenses"] - df["Revenue"]
+
+df["CAC"] = df["Marketing_Spend"] / df["New_Customers"]
+
+df["Avg_Revenue_per_Customer"] = df["Revenue"] / (df["New_Customers"] + df["Retained_Customers"])
+
+
+# Adjust the lifetime multiplier if you prefer 9 or 12 months
+MONTHS_OF_LIFETIME = 6
+df["LTV"] = df["Avg_Revenue_per_Customer"] * MONTHS_OF_LIFETIME
+
+df["LTV_CAC_Ratio"] = df["LTV"] / df["CAC"]
+
+df["Run_Rate"] = df["Revenue"] * 12
+
+# month column
+df["YearMonth"] = df["Date"].dt.to_period("M")
+
+print(df)
+
+df.to_csv(f"{base}/finance_data.csv")
